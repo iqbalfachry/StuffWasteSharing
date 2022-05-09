2ed0bb17ad090827d25fbb7f54401752c40be650
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-10 00:22:17 +0700

    add menu item

--- app/src/main/java/com/example/stuffy/presentation/menu/MenuActivity.kt
+++ app/src/main/java/com/example/stuffy/presentation/menu/MenuActivity.kt
@@ -1,10 +1,12 @@
 package com.example.stuffy.presentation.menu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import com.example.stuffy.R
 import com.example.stuffy.databinding.ActivityMenuBinding
+import com.example.stuffy.presentation.main.MainActivity
 
 class MenuActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMenuBinding
@@ -17,6 +19,14 @@ class MenuActivity : AppCompatActivity() {
                 .load(R.drawable.user1)
                 .circleCrop()
                 .into(imageView3)
+        }
+        binding.imageView2.setOnClickListener {
+            Intent(this@MenuActivity, MainActivity::class.java).also{
+                startActivity(it)
+            }
+
+
+
         }
     }
 }
