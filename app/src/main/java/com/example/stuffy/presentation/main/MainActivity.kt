c15a79a5fee69207bfc841c2cd92a7d98a502d9c
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-10 21:57:25 +0700

    add animation home to menu

--- app/src/main/java/com/example/stuffy/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/stuffy/presentation/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.stuffy.presentation.main
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
 
 
 import androidx.navigation.findNavController
@@ -31,6 +32,31 @@ class MainActivity : AppCompatActivity() {
 
 
         navView.setupWithNavController(navController)
+        val options = NavOptions.Builder()
+            .setLaunchSingleTop(true)
+            .setEnterAnim(R.anim.slide_in_left)
+            .setExitAnim(R.anim.slide_out_right)
+            .setPopEnterAnim(R.anim.slide_in_right)
+            .setPopExitAnim(R.anim.slide_out_left)
+            .setPopUpTo(navController.graph.startDestinationId, true)
+            .build()
+        navView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.navigation_home -> {
+                    navController.navigate(R.id.navigation_home,null,options)
+                }
+                R.id.navigation_dashboard -> {
+                    navController.navigate(R.id.navigation_dashboard,null,options)
+                }
+                R.id.navigation_share -> {
+                    navController.navigate(R.id.navigation_share,null,options)
+                }
+                R.id.navigation_notifications -> {
+                    navController.navigate(R.id.navigation_notifications,null,options)
+                }
+            }
+            true
+        }
 
 
     }
