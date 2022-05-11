80b6f9c52a5fbe5d80fb881f15856ba65ec2d759
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-11 13:35:03 +0700

    add splash activity

--- app/src/main/java/com/example/stuffy/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/example/stuffy/presentation/splash/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.example.stuffy.presentation.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import com.example.stuffy.databinding.ActivityMenuBinding
 import com.example.stuffy.databinding.ActivitySplashBinding
+import com.example.stuffy.presentation.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
@@ -11,5 +15,16 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        Handler(mainLooper).postDelayed({
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+            }
+            finish()
+        }, 2000)
     }
 }
