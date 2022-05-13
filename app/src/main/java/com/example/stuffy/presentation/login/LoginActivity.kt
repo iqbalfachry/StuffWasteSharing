c64b67a8484785e5076491957d324b7a252ea75d
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-13 16:18:00 +0700

    add favorite activity

---
+++ app/src/main/java/com/example/stuffy/presentation/login/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.stuffy.presentation.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stuffy.databinding.ActivityLoginBinding
+import com.example.stuffy.databinding.ActivitySettingsBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
