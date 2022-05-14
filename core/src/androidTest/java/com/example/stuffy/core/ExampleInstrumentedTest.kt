b15edba342a89ab2357ef4c92952ee65ee4db9f4
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-14 23:22:53 +0700

    fix styling

--- core/src/androidTest/java/com/example/stuffy/core/ExampleInstrumentedTest.kt
+++ core/src/androidTest/java/com/example/stuffy/core/ExampleInstrumentedTest.kt
@@ -21,6 +21,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.stuffy", appContext.packageName)
+        assertEquals("com.example.stuffy.core.test", appContext.packageName)
     }
 }
