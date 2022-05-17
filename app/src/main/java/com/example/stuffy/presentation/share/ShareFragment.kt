1861b4c4dac74878f1979403b0d3999bd6213dff
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-17 13:17:38 +0700

    fix confirmation

--- app/src/main/java/com/example/stuffy/presentation/share/ShareFragment.kt
+++ app/src/main/java/com/example/stuffy/presentation/share/ShareFragment.kt
@@ -3,10 +3,12 @@ package com.example.stuffy.presentation.share
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.View
 
 import android.view.ViewGroup
 
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.stuffy.databinding.FragmentShareBinding
 
@@ -14,6 +16,7 @@ import com.example.stuffy.databinding.FragmentShareBinding
 class ShareFragment :  Fragment() {
 
     private var _binding: FragmentShareBinding? = null
+    private val shareViewModel : ShareViewModel by viewModels()
 
 
     private val binding get() = _binding
@@ -23,21 +26,24 @@ class ShareFragment :  Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): ConstraintLayout? {
-        val shareViewModel =
-            ViewModelProvider(this)[ShareViewModel::class.java]
 
         _binding = FragmentShareBinding.inflate(inflater, container, false)
 
-binding?.count?.setText(shareViewModel.count.value.toString())
+
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.count?.text = shareViewModel.count.value.toString()
         binding?.inc?.setOnClickListener{
             shareViewModel.inc()
-            binding?.count?.setText(shareViewModel.count.value.toString())
+            binding?.count?.text = shareViewModel.count.value.toString()
         }
         binding?.dec?.setOnClickListener{
             shareViewModel.dec()
-            binding?.count?.setText(shareViewModel.count.value.toString())
+            binding?.count?.text = shareViewModel.count.value.toString()
         }
-        return binding?.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/stuffy/presentation/share/ShareViewModel.kt
+++ app/src/main/java/com/example/stuffy/presentation/share/ShareViewModel.kt
@@ -5,27 +5,25 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ShareViewModel : ViewModel() {
-    private val _count  = MutableLiveData<Int>().apply {
+    private val _count = MutableLiveData<Int>().apply {
         value = 1
     }
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is share Fragment"
-    }
-    val count: LiveData<Int> = _count
-   fun inc (){
-       _count.value?.let {
 
-           _count.value = it  + 1
-       }
-   }
-    fun dec (){
+    val count: LiveData<Int> = _count
+    fun inc() {
         _count.value?.let {
-            if(_count.value!=1){
-                _count.value = it  - 1
+            _count.value = it + 1
+        }
+    }
+
+    fun dec() {
+        _count.value?.let {
+            if (_count.value != 1) {
+                _count.value = it - 1
             }
 
         }
     }
-    val text: LiveData<String> = _text
+
 }
