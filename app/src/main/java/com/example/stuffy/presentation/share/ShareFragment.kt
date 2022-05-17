667c8fd4b985d5db71fef42818db99b9a06dd2b2
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-17 17:50:24 +0700

    fix confirmation

--- app/src/main/java/com/example/stuffy/presentation/share/ShareFragment.kt
+++ app/src/main/java/com/example/stuffy/presentation/share/ShareFragment.kt
@@ -1,23 +1,32 @@
 package com.example.stuffy.presentation.share
 
+
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
-
 import android.view.ViewGroup
-
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
+import com.example.stuffy.core.utils.createTempFile
+import com.example.stuffy.core.utils.uriToFile
 import com.example.stuffy.databinding.FragmentShareBinding
+
+import java.io.File
 
 
 class ShareFragment :  Fragment() {
 
     private var _binding: FragmentShareBinding? = null
     private val shareViewModel : ShareViewModel by viewModels()
-
+    private var getFile: File? = null
 
     private val binding get() = _binding
 
@@ -44,8 +53,61 @@ class ShareFragment :  Fragment() {
             shareViewModel.dec()
             binding?.count?.text = shareViewModel.count.value.toString()
         }
+        binding?.camera?.setOnClickListener{
+            startGallery()
+        }
+
+    }
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = activity?.let { uriToFile(selectedImg, it) }
+            getFile = myFile
+
+            binding?.camera?.setImageURI(selectedImg)
+        }
     }
 
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        activity?.let { intent.resolveActivity(it.packageManager) }
+        activity?.application?.let {
+            createTempFile(it).also {
+                val photoURI: Uri? = activity?.let { it1 ->
+                    FileProvider.getUriForFile(
+                        it1,
+                        "com.example.stuffy",
+                        it
+                    )
+                }
+                currentPhotoPath = it.absolutePath
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                launcherIntentCamera.launch(intent)
+            }
+        }
+    }
+    private lateinit var currentPhotoPath: String
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            getFile = myFile
+            val result = BitmapFactory.decodeFile(myFile.path)
+            binding?.camera?.setImageBitmap(result)
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
