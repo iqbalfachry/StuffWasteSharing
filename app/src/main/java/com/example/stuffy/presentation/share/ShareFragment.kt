f5e8d70a5f7f12cf8cc4ee24950e336ac56506d4
iqbalfachry19 <ifachry@rocketmail.com>
2022-06-02 23:41:58 +0700

    fix home

--- app/src/main/java/com/example/stuffy/presentation/share/ShareFragment.kt
+++ app/src/main/java/com/example/stuffy/presentation/share/ShareFragment.kt
@@ -1,26 +1,36 @@
 package com.example.stuffy.presentation.share
 
 
+import android.Manifest
 import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.location.Geocoder
+import android.location.Location
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 
 import com.example.stuffy.core.utils.createTempFile
 import com.example.stuffy.core.utils.uriToFile
 import com.example.stuffy.databinding.FragmentShareBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 import java.io.File
+import java.util.*
 
 
 class ShareFragment :  Fragment() {
@@ -28,9 +38,10 @@ class ShareFragment :  Fragment() {
     private var _binding: FragmentShareBinding? = null
     private val shareViewModel : ShareViewModel by viewModel()
     private var getFile: File? = null
-
+    private var fusedLocationClient: FusedLocationProviderClient?=null
     private val binding get() = _binding
-
+    private var geocoder:Geocoder?=null
+    private lateinit var location: Location
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,12 +50,14 @@ class ShareFragment :  Fragment() {
 
         _binding = FragmentShareBinding.inflate(inflater, container, false)
 
-
+        fusedLocationClient = activity?.let { LocationServices.getFusedLocationProviderClient(it) }
+        geocoder = Geocoder(activity, Locale.getDefault())
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        getMyLastLocation()
         binding?.count?.text = shareViewModel.count.value.toString()
         binding?.inc?.setOnClickListener{
             shareViewModel.inc()
@@ -63,6 +76,66 @@ class ShareFragment :  Fragment() {
         }
 
     }
+    private fun checkPermission(permission: String): Boolean {
+        return activity?.let {
+            ContextCompat.checkSelfPermission(
+                it,
+                permission
+            )
+        } == PackageManager.PERMISSION_GRANTED
+    }
+    private fun setLocation(location: Location){
+        this.location = location
+
+    }
+    private fun getMyLastLocation() {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) &&
+            checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+        ) {
+            fusedLocationClient?.lastLocation?.addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    setLocation(location)
+               binding?.textView62?.setText( geocoder?.getFromLocation(location.latitude, location.longitude, 1)?.get(0)?.getAddressLine(0).toString())
+
+                } else {
+                    Toast.makeText(
+                        activity,
+                        "Location is not found. Try Again",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+            }
+        } else {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+
+        }
+
+    }
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false -> {
+                    // Precise location access granted.
+                    getMyLastLocation()
+                }
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false -> {
+                    // Only approximate location access granted.
+                    getMyLastLocation()
+                }
+                else -> {
+                    // No location access granted.
+                }
+            }
+        }
+
     private val launcherIntentGallery = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
